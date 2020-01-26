bc033718a7c751fd61e7765506bf559171600ce0
Maxim Ovodok <maksim.avadok@itechart-group.com>
2020-01-27 00:22:06 +0300

    1. Returned old version of Tag entity

--- NetCoreNotesApp.DAL/Entities/Tag.cs
+++ NetCoreNotesApp.DAL/Entities/Tag.cs
@@ -1,14 +1,9 @@
-using System.Collections.Generic;
-
 namespace NetCoreNotesApp.DAL.Entities
 {
     public class Tag : BaseEntity
     {
         public string Name { get; set; }
-        public ICollection<Note> Notes { get; set; }
-        public Tag()
-        {
-            Notes = new HashSet<Note>();
-        }
+        public int NoteId { get; set; }
+        public Note Note { get; set; }
     }
 }
